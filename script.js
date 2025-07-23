9797fb14ac1c0a56f80745237dfb26f999f24a7c
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-07-23 16:01:10 +0500

    indentation fixes

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  let h1 = document.querySelector("h1");
+  let rendColor = getrandColor();
+  h1.innerText = rendColor;
+  h1.style.color = rendColor;
 
-let btn  = document.querySelector("button")
-btn.addEventListener("click", function(){
-    let h1 = document.querySelector("h1")
-    let rendColor = getrandColor();
-    h1.innerText = rendColor;
-    h1.style.color = rendColor;
-    
-    console.log("Color Updated")
+  console.log("Color Updated");
 
-    let div = document.querySelector(".color"); 
-    div.style.backgroundColor  =  rendColor;
+  let div = document.querySelector(".color");
+  div.style.backgroundColor = rendColor;
+});
 
-})
+function getrandColor() {
+  let red = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
 
-function getrandColor(){
-    let red = Math.floor(Math.random() * 255)
-    let blue = Math.floor(Math.random() * 255)
-    let green = Math.floor(Math.random() * 255)
-
-    let color = `rgb(${red}, ${blue}, ${green})`
-    return color
+  let color = `rgb(${red}, ${blue}, ${green})`;
+  return color;
 }
 
+let box = document.querySelector(".box");
 
-let box = document.querySelector(".box")
-
-box.addEventListener("mousemove", function(){
-    box.innerText = "Mouse Inside Box";
-})
+box.addEventListener("mousemove", function () {
+  box.innerText = "Mouse Inside Box";
+});
