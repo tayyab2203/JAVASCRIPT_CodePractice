2f44ee1e0dfcc37aebfca1d0f978522eea695bd7
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-07-23 15:31:38 +0500

    color spelling change

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let btn  = document.querySelector("button")
+btn.addEventListener("click", function(){
+    let h1 = document.querySelector("h1")
+    let rendColor = getrandColor();
+    h1.innerText = rendColor;
+    h1.style.color = rendColor;
+    
+    console.log("Color Updated")
+
+    let div = document.querySelector(".color"); 
+    div.style.backgroundColor =  rendColor;
+
+})
+
+function getrandColor(){
+    let red = Math.floor(Math.random() * 255)
+    let blue = Math.floor(Math.random() * 255)
+    let green = Math.floor(Math.random() * 255)
+
+    let color = `rgb(${red}, ${blue}, ${green})`
+    return color
+}
