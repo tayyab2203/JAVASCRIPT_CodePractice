d510fb7f6d10ecbfb6b5a095a97899b078670e55
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-07-23 15:22:23 +0500

    This is random color generator

--- Events/script.js
+++ Events/script.js
@@ -1,13 +1,25 @@
-let btns = document.querySelectorAll("button")
 
-for (btn of btns) {
-    // btn.onclick = sayHello;
-    btn.addEventListener("click", sayHello);
-    btn.addEventListener("click", sayName);
-}
-function sayHello(){
-    alert("Heloo Tayyab");
-}
-function sayName(){
-    alert("This is JS Name Function");
+let btn  = document.querySelector("button")
+
+
+
+btn.addEventListener("click", function(){
+    let h1 = document.querySelector("h1")
+    let rendColor = getrandColor();
+    h1.innerText = rendColor
+    
+    console.log("COlor Updated")
+
+    let div = document.querySelector(".color"); 
+    div.style.backgroundColor =  rendColor;
+
+})
+
+function getrandColor(){
+    let red = Math.floor(Math.random() * 255)
+    let blue = Math.floor(Math.random() * 255)
+    let green = Math.floor(Math.random() * 255)
+
+    let color = `rgb(${red}, ${blue}, ${green})`
+    return color
 }
